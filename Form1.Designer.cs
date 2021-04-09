8520084f575157e4590277fa91589be71fcb2843
Emre ŞAHİN <78268357+emresahin52@users.noreply.github.com>
2021-04-09 18:30:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace SinemaUygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblmisir = new System.Windows.Forms.Label();
+            this.lblcay = new System.Windows.Forms.Label();
+            this.lblsu = new System.Windows.Forms.Label();
+            this.lblbilet = new System.Windows.Forms.Label();
+            this.txtbilet = new System.Windows.Forms.TextBox();
+            this.txtsu = new System.Windows.Forms.TextBox();
+            this.txtcay = new System.Windows.Forms.TextBox();
+            this.txtmisir = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbltutar = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblkasa = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Lime;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1130, 172);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.SandyBrown;
+            this.groupBox2.Controls.Add(this.txtmisir);
+            this.groupBox2.Controls.Add(this.txtcay);
+            this.groupBox2.Controls.Add(this.txtsu);
+            this.groupBox2.Controls.Add(this.txtbilet);
+            this.groupBox2.Controls.Add(this.lblbilet);
+            this.groupBox2.Controls.Add(this.lblsu);
+            this.groupBox2.Controls.Add(this.lblcay);
+            this.groupBox2.Controls.Add(this.lblmisir);
+            this.groupBox2.Location = new System.Drawing.Point(0, 180);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox2.Size = new System.Drawing.Size(357, 361);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ürünler";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.groupBox3.Controls.Add(this.label13);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(383, 180);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox3.Size = new System.Drawing.Size(357, 361);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Fiyat Listesi";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Yellow;
+            this.groupBox4.Controls.Add(this.lblkasa);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.lbltutar);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Location = new System.Drawing.Point(772, 180);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox4.Size = new System.Drawing.Size(357, 361);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Tutar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 549);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1130, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblmisir
+            // 
+            this.lblmisir.AutoSize = true;
+            this.lblmisir.Location = new System.Drawing.Point(93, 115);
+            this.lblmisir.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblmisir.Name = "lblmisir";
+            this.lblmisir.Size = new System.Drawing.Size(40, 18);
+            this.lblmisir.TabIndex = 0;
+            this.lblmisir.Text = "Mısır";
+            // 
+            // lblcay
+            // 
+            this.lblcay.AutoSize = true;
+            this.lblcay.Location = new System.Drawing.Point(99, 163);
+            this.lblcay.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblcay.Name = "lblcay";
+            this.lblcay.Size = new System.Drawing.Size(34, 18);
+            this.lblcay.TabIndex = 1;
+            this.lblcay.Text = "Çay";
+            // 
+            // lblsu
+            // 
+            this.lblsu.AutoSize = true;
+            this.lblsu.Location = new System.Drawing.Point(107, 211);
+            this.lblsu.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblsu.Name = "lblsu";
+            this.lblsu.Size = new System.Drawing.Size(26, 18);
+            this.lblsu.TabIndex = 2;
+            this.lblsu.Text = "Su";
+            // 
+            // lblbilet
+            // 
+            this.lblbilet.AutoSize = true;
+            this.lblbilet.Location = new System.Drawing.Point(40, 67);
+            this.lblbilet.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblbilet.Name = "lblbilet";
+            this.lblbilet.Size = new System.Drawing.Size(93, 18);
+            this.lblbilet.TabIndex = 3;
+            this.lblbilet.Text = "Sinema Bileti";
+            // 
+            // txtbilet
+            // 
+            this.txtbilet.Location = new System.Drawing.Point(141, 64);
+            this.txtbilet.Name = "txtbilet";
+            this.txtbilet.Size = new System.Drawing.Size(100, 24);
+            this.txtbilet.TabIndex = 4;
+            // 
+            // txtsu
+            // 
+            this.txtsu.Location = new System.Drawing.Point(141, 205);
+            this.txtsu.Name = "txtsu";
+            this.txtsu.Size = new System.Drawing.Size(100, 24);
+            this.txtsu.TabIndex = 5;
+            // 
+            // txtcay
+            // 
+            this.txtcay.Location = new System.Drawing.Point(141, 160);
+            this.txtcay.Name = "txtcay";
+            this.txtcay.Size = new System.Drawing.Size(100, 24);
+            this.txtcay.TabIndex = 6;
+            this.txtcay.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtmisir
+            // 
+            this.txtmisir.Location = new System.Drawing.Point(141, 112);
+            this.txtmisir.Name = "txtmisir";
+            this.txtmisir.Size = new System.Drawing.Size(100, 24);
+            this.txtmisir.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(534, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(365, 30);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ŞAHIN SINEMA SALONU";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SinemaUygulaması.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 18);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Sinema Bileti";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(210, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 18);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "14 TL";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(66, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 18);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Mısır(Orta Boy)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(141, 163);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 18);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Çay";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(112, 208);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 18);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Su(0.5lt)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(210, 115);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(37, 18);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "8 TL";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(210, 166);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(37, 18);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "3 TL";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(210, 208);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(49, 18);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "1.5 TL";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tutarınız:";
+            // 
+            // lbltutar
+            // 
+            this.lbltutar.AutoSize = true;
+            this.lbltutar.Location = new System.Drawing.Point(141, 112);
+            this.lbltutar.Name = "lbltutar";
+            this.lbltutar.Size = new System.Drawing.Size(45, 18);
+            this.lbltutar.TabIndex = 9;
+            this.lbltutar.Text = "00 TL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Toplam Kasa";
+            // 
+            // lblkasa
+            // 
+            this.lblkasa.AutoSize = true;
+            this.lblkasa.Location = new System.Drawing.Point(153, 248);
+            this.lblkasa.Name = "lblkasa";
+            this.lblkasa.Size = new System.Drawing.Size(45, 18);
+            this.lblkasa.TabIndex = 11;
+            this.lblkasa.Text = "00 TL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1134, 620);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtmisir;
+        private System.Windows.Forms.TextBox txtcay;
+        private System.Windows.Forms.TextBox txtsu;
+        private System.Windows.Forms.TextBox txtbilet;
+        private System.Windows.Forms.Label lblbilet;
+        private System.Windows.Forms.Label lblsu;
+        private System.Windows.Forms.Label lblcay;
+        private System.Windows.Forms.Label lblmisir;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblkasa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltutar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinemaUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int toplamkasa=0;
+        private void button1_Click(object sender, EventArgs e)
+        {   int toplamfiyat=0;
+           
+            int bilet, su, cay, misir;
+         
+            bilet = Convert.ToInt16(txtbilet.Text);
+            su = Convert.ToInt16(txtsu.Text);
+            cay = Convert.ToInt16(txtcay.Text);
+            misir = Convert.ToInt16(txtmisir.Text);
+            toplamfiyat=bilet*14 + misir*8 +su*2 +  cay*3;
+            toplamkasa = toplamkasa + toplamfiyat;
+            lbltutar.Text = toplamfiyat.ToString(); // toplam fiyatı labela aktarıyorum
+            lblkasa.Text = toplamkasa.ToString();
+
+             
+          
+   
+       
+        }
+    }
+}
